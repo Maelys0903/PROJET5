88990309b6e200966a0b533e6e783cba76b1dc40
MaëlysD <maelys0903@orange.fr>
2025-08-06 16:09:08 +0200

    Ajout du modèle BERT local + MLP + chargement corrigé

--- app/bert.py
+++ app/bert.py
@@ -3,13 +3,14 @@
 import re
 import joblib
 import mlflow.pyfunc
+import pandas as pd
 
 from typing import List
 
-# Nom du modèle dans le MLflow Model Registry
-MODEL_NAME = "model"
-MODEL_STAGE = "Production"
-MLB_PATH = "mlb.pkl"
+import os
+
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "model_bert")
+MLB_PATH = os.path.join(os.path.dirname(__file__), "mlb.pkl")
 
 # Variables globales
 pipeline = None
@@ -20,8 +21,7 @@ def load_model_once():
     global pipeline, mlb
     if pipeline is None or mlb is None:
         # Chargement depuis le Model Registry
-        model_uri = f"models:/{MODEL_NAME}/{MODEL_STAGE}"
-        pipeline = mlflow.pyfunc.load_model(model_uri)
+        pipeline = mlflow.pyfunc.load_model(MODEL_PATH)
         mlb = joblib.load(MLB_PATH)
 
 def clean_text(text: str) -> str:
@@ -31,11 +31,14 @@ def clean_text(text: str) -> str:
     return text.strip()
 
 def predict_tags(text: str) -> List[str]:
-    """Prédit les tags à partir du texte d'entrée."""
-    if not text.strip():
-        return []
-    load_model_once()
-    cleaned = clean_text(text)
-    df_input = {"text": [cleaned]} 
-    prediction = pipeline.predict(df_input)
-    return mlb.inverse_transform(prediction)[0]
+    try:
+        if not text.strip():
+            return []
+        load_model_once()
+        cleaned = clean_text(text)
+        df_input = pd.DataFrame({"text": [cleaned]})
+        prediction = pipeline.predict(df_input)
+        return mlb.inverse_transform(prediction)[0]
+    except Exception as e:
+        print(f"Erreur dans predict_tags : {e}")
+        raise
